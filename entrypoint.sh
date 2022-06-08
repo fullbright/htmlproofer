2a29b5a703982c824b47eb36a026592e7d49e9a2
fullbright <full3right@gmail.com>
2022-06-08 16:43:56 +0200

    Rewrite the executable

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,5 @@ htmlproofer -v
 
 echo "Running htmlproofer with arguments ${INPUT_ARGUMENTS}"
 echo "Export the result into a file ${INPUT_OUTPUT_FILE}"
-echo ${INPUT_ARGUMENTS} | xargs htmlproofer "${INPUT_DIRECTORY}" >> ${INPUT_OUTPUT_FILE} 2>$1
+#echo ${INPUT_ARGUMENTS} | xargs htmlproofer "${INPUT_DIRECTORY}" >> ${INPUT_OUTPUT_FILE} 2>$1
+htmlproofer ${INPUT_ARGUMENTS} "${INPUT_DIRECTORY}" >> ${INPUT_OUTPUT_FILE} 2>$1
