875cb591ace6699d64ead2bd2c93001d8e10c5de
fullbright <full3right@gmail.com>
2022-06-08 15:53:42 +0200

    Redirect stderr and stdout to file and console

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ htmlproofer -v
 
 echo "Running htmlproofer with arguments ${INPUT_ARGUMENTS}"
 echo "Export the result into a file ${INPUT_OUTPUT_FILE}"
-echo ${INPUT_ARGUMENTS} | xargs htmlproofer "${INPUT_DIRECTORY}" >> ${INPUT_OUTPUT_FILE}
+echo ${INPUT_ARGUMENTS} | xargs htmlproofer "${INPUT_DIRECTORY}" |& tee -a ${INPUT_OUTPUT_FILE}
